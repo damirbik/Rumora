2366352df8ceeda20d863220312870aa4c79fbd4
damirbik <159629681+damirbik@users.noreply.github.com>
2025-10-12 18:11:14 +0300

    Пустой каркас(запускается)
    Используемые инструменты
     *Spring Data JPA
    * Spring Boot DevTools
    * Docker Compose Support
    * Thymeleaf
    * Spring Web

---
+++ Rumora_demo/src/main/java/org/lamdateam/rumora_demo/RumoraDemoApplication.java
@@ -0,0 +1,13 @@
+package org.lamdateam.rumora_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumoraDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RumoraDemoApplication.class, args);
+    }
+
+}
---
+++ Rumora_demo/src/test/java/org/lamdateam/rumora_demo/RumoraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lamdateam.rumora_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumoraDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
