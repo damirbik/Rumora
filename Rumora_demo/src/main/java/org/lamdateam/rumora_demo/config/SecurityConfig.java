87a3c1aaa3dff2855d34ba19dfa8b0a2f6833c7e
damirbik <dbikchurayev@bk.ru>
2025-12-23 22:52:31 +0300

    Update SecurityConfig.java

--- Rumora_demo/src/main/java/org/lamdateam/rumora_demo/config/SecurityConfig.java
+++ Rumora_demo/src/main/java/org/lamdateam/rumora_demo/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://127.0.0.1:3000", "https://remjest-rumora-5a51.twc1.net"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
